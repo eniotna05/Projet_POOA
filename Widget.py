7338d697787cf1bcaec53d88f8bd7360feffa0ab
Yoann Gauthier <yoann.gauthier@nebulae.co>
2017-10-10 14:10:32 +0200

    First version of the graphic interface, allowing to draw rectangles on a panel

---
+++ Widget.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+
+
+class MyPaintWidget(Widget):
+
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
+
+class MyPaintApp(App):
+
+    def build(self):
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
+
+
+if __name__ == '__main__':
+    MyPaintApp().run()
