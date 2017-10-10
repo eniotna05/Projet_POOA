c2066c3a4f686f62fcdf62b3e02622e9128a118d
eniotna05 <antoine.aubay@gmail.com>
2017-10-10 14:07:27 +0200

    Version avec les couleurs

--- Dictionnaire_objet.py
+++ Dictionnaire_objet.py
@@ -2,6 +2,41 @@
 abscisse_max = 10000
 ordonne_max = 10000
 
+Alphabet =
+{1:A
+ 2:
+
+
+
+
+
+
+    }
+
+
+
+class Color:
+
+    def __init__(self, R, G, B):
+
+        
+        if (not isinstance(R, int) ) or (not isinstance(G, int) ) or (not isinstance(B, int) ):
+            raise TypeError("Tous les parametres doivent etre des entiers")
+        if R < 0 or R > 255 or G < 0 or G > 255 or B < 0 or B > 255:
+            raise ValueError("Tous les parametres doivent etre entre 0 et 255")
+  
+        self.R = R
+        self.G = G
+        self.B = B
+        
+    def __repr__(self):
+        
+        return """Couleur de code RGB: {}.{}.{}""".format(self.R, self.G, self.B)
+
+
+white = Color(255,255,255)
+
+black = Color(0,0,0)
 
 class Point:
 
@@ -33,30 +68,50 @@ class Point:
 
 
 class Ligne:
-    def __init__(self, point_A, point_B):
+    def __init__(self, point_A, point_B,color = black):
         if not isinstance(point_A, Point):
             raise TypeError("Le premier parametre doit être un point")
         if not isinstance(point_B, Point):
             raise TypeError("Le deuxieme parametre doit être un point")
+        if not isinstance(color, Color):
+            raise TypeError("Le troisieme parametre doit être une couleur")
         self.point_A = point_A
         self.point_B = point_B
+        self.color = color
 
     def __repr__(self):
         
-            return """Ligne entre le {} et le {}""".format(self.point_A, self.point_B)
-
+        return """Ligne entre le {} et le {} de couleur {}""".format(self.point_A, self.point_B, self.color)
 
 
 class Polygone:
-    def __init__(self, point_A, point_B, point_C, point_supplementaire*):
-        if not isinstance(point_A, Point):
-            raise TypeError("Le premier parametre doit être un point")
-        if not isinstance(point_B, Point):
-            raise TypeError("Le deuxieme parametre doit être un point")
+    
+    def __init__(self, point_A, point_B, point_C, *points_supplementaires, color = black):
+
+        if (not isinstance(point_A, Point)) or (not isinstance(point_B, Point)) or (not isinstance(point_C, Point)):
+            raise TypeError("Tous les parametres doivent etre des points")
+        for k in points_supplentaires:
+            if not isinstance(k, Point):
+                raise TypeError("Tous les parametres doivent etre des points")
+        if not isinstance(color, Color):
+            raise TypeError("Le parametre couleur doit être une couleur")
+        
         self.point_A = point_A
         self.point_B = point_B
+        self.point_C = point_C
+        for k in point_supplementaires:
+            self.point 
+            
+        self.color = color
+        
 
     def __repr__(self):
         
-            return """Ligne entre le {} et le {}""".format(self.point_A, self.point_B)  
+        return """Polygone de point""".format(self.point_A, self.point_B)
+
+
+
+
+
+
 
